8f9bbb64b28166ec827af2c9ea9f73c3f3ba4063
Leunamz <eliudniebles@gmail.com>
2023-10-11 20:03:27 -0500

    se configuro mason lsp y auto completacion

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,42 +1,44 @@
 return {
-   "williamboman/mason.nvim",
-  enabled= false,
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-
-  },
-
-  config = function()
-    local mason = require("mason")
-
-    local mason_lspconfig = require("mason-lspconfig")
-
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
-        }
-      }
-    })
-
-  mason_lspconfig.setup({
-    ensure_installed = {
-      "tsserver",
-      "html",
-      "cssls",
-      "lua_ls",
-      "emmet_ls",
-      "pyright",
-      "phpactor",
-      "sqlls",
-      "yamlls",
-      "jdtls",
-      "omnisharp_mono"
-    },
-    -- auto install configured servers(with lsconfig)
-    automatic_installation = true, -- not the same as ensure_installed
-  })
-  end,
+  "williamboman/mason.nvim",
+  config = true,
 }
+ -- enabled= false,
+ -- dependencies = {
+ --   "williamboman/mason-lspconfig.nvim",
+ --
+ -- },
+ --
+ -- config = function()
+ --   local mason = require("mason")
+ --
+ --   local mason_lspconfig = require("mason-lspconfig")
+ --
+ --   mason.setup({
+ --     ui = {
+ --       icons = {
+ --         package_installed = "✓",
+ --         package_pending = "➜",
+ --         package_uninstalled = "✗"
+ --       }
+ --     }
+ --   })
+ --
+ -- mason_lspconfig.setup({
+ --   ensure_installed = {
+ --     "tsserver",
+ --     "html",
+ --     "cssls",
+ --     "lua_ls",
+ --     "emmet_ls",
+ --     "pyright",
+ --     "phpactor",
+ --     "sqlls",
+ --     "yamlls",
+ --     "jdtls",
+ --     "omnisharp_mono"
+ --   },
+ --   -- auto install configured servers(with lsconfig)
+ --   automatic_installation = true, -- not the same as ensure_installed
+ -- })
+ -- end,
+--}
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,49 +1,64 @@
 return {
   "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  enabled= false,
   dependencies = {
-    "hrsh7th/cmp-buffer", -- autocomleta con datos del buffer
-    "hrsh7th/cmp-path", -- nos permite obtener recomencaciones para ruta del sistemas
-    "L3MON4D3/LuaSnip", -- snippet engine
-    "rafamadriz/friendly-snippets", -- useful snippets
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-git",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "windwp/nvim-autopairs",
   },
+  event = "VeryLazy",
+  main = "config.plugins.cmp",
+  config = true,
 
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-
-    -- load vscode style snippets fron installed plugins 
-    require("luasnip.loaders.from_vscode").lazy_load()
-
-    cmp.setup({
-      completacion = {
-        completeopt = "menu,menuone,preview,noselect",
-      },
-      snippet = { -- configure how nvim cmp  interacts with snippet engine
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- prev suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(),-- next suggestion
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({select = false}),
-
-      }),
-
-      -- source for autocompletion
-      sources = cmp.config.sources({
-        {name = "nvim_lsp"},-- Lsp
-        {name = "luasnip"}, -- snippets
-        {name = "buffer"},-- text within current buffer
-        {name = "path"}, -- file system paths
-      }),
-
-    })
-  end,
+-- "hrsh7th/nvim-cmp",
+-- event = "InsertEnter",
+-- enabled= false,
+-- dependencies = {
+--   "hrsh7th/cmp-buffer", -- autocomleta con datos del buffer
+--   "hrsh7th/cmp-path", -- nos permite obtener recomencaciones para ruta del sistemas
+--   "L3MON4D3/LuaSnip", -- snippet engine
+--   "rafamadriz/friendly-snippets", -- useful snippets
+-- },
+--
+-- config = function()
+--   local cmp = require("cmp")
+--   local luasnip = require("luasnip")
+--
+--   -- load vscode style snippets fron installed plugins 
+--   require("luasnip.loaders.from_vscode").lazy_load()
+--
+--   cmp.setup({
+--     completacion = {
+--       completeopt = "menu,menuone,preview,noselect",
+--     },
+--     snippet = { -- configure how nvim cmp  interacts with snippet engine
+--       expand = function(args)
+--         luasnip.lsp_expand(args.body)
+--       end,
+--     },
+--     mapping = cmp.mapping.preset.insert({
+--       ["<C-k>"] = cmp.mapping.select_prev_item(), -- prev suggestion
+--       ["<C-j>"] = cmp.mapping.select_next_item(),-- next suggestion
+--       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+--       ["<C-f>"] = cmp.mapping.scroll_docs(4),
+--       ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+--       ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+--       ["<CR>"] = cmp.mapping.confirm({select = false}),
+--
+--     }),
+--
+--     -- source for autocompletion
+--     sources = cmp.config.sources({
+--       {name = "nvim_lsp"},-- Lsp
+--       {name = "luasnip"}, -- snippets
+--       {name = "buffer"},-- text within current buffer
+--       {name = "path"}, -- file system paths
+--     }),
+--
+--   })
+-- end,
 }
